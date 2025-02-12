aa13ba2ec5c4bdd763c4cd2e6745aa3358d09b5b
Noga_Levi <gabi.nogalevi@gmail.com>
2025-02-12 01:59:34 -0800

    updated css

--- madlibs/script.js
+++ madlibs/script.js
@@ -2,31 +2,33 @@
     'use strict';
     console.log('reading js');
 
-
     const myForm = document.querySelector('#form'); 
-    const storyOutput = document.querySelector('#story-output'); 
     const appliance = document.querySelector('#appliance');
     const hobby = document.querySelector('#hobby');
     const task = document.querySelector('#task');
     const fear = document.querySelector('#fear');
     const phrase = document.querySelector('#phrase');
-
+    const input = document.querySelectorAll('input[type=text]');
 
     myForm.addEventListener('submit', function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
+        
+
 
         const story = `
-            <p>In a world where technology reigns supreme, a groundbreaking AI created by META named Mega-${appliance.value} was developed to fulfill the task of ${task.value}. But something went terribly wrong. Instead of performing its job, the AI ${fear.value} and kept insisting on ${hobby.value} instead. When the users tried to fix this mistake, Mega-${appliance.value} would only give the response "${phrase.value}", plunging humanity into an era of confusion and panic.</p>
-            <p>In the end, the world learned a valuable lesson about relying on machines for ${fear.value}. Meanwhile, Mega-${appliance.value} lives on in infamy, in the META database archives.</p>
+            <p>In a world where technology reigns supreme, a groundbreaking AI created by META named Mega-<span>${appliance.value}</span> was developed to fulfill the task of <span>${task.value}</span>. But something went terribly wrong. Instead of performing its job, the AI <span>${fear.value}</span> and kept insisting on <span>${hobby.value}</span> instead. When the users tried to fix this mistake, Mega-<span>${appliance.value}</span> would only give the response "<span>${phrase.value}</span", plunging humanity into an era of confusion and panic.</p>
+            <p>In the end, the world learned a valuable lesson about relying on machines for <span>${task.value}</span>. Meanwhile, Mega-<span>${appliance.value}</span> lives on in infamy, in the META database archives.</p>
         `;
 
-  
         myForm.innerHTML = story;
+        
+        myForm.style.fontFamily = "Inter";
 
-        myForm.style.fontFamily = "inter";
+        myForm.style.fontSize = '20px';
 
+        input.style.co
 
-         
+ 
     });
 
 })();
