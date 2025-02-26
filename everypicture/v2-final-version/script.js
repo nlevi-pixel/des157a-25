6ab74876fb3ba0841c7f41335555ce84d3c2b088
Noga_Levi <gabi.nogalevi@gmail.com>
2025-02-25 22:09:40 -0800

    final EP

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -1,17 +1,19 @@
 (function(){
+    const sections = document.querySelectorAll("article.accordion section div");
 
+    sections.forEach((section) => {
+        const images = section.querySelectorAll("img");
 
+        images.forEach((img) => {
+            img.addEventListener("click", function () {
+                const visibleImages = section.querySelectorAll("img:not([style*='display: none'])");
 
+                if (visibleImages.length > 1) {
+                    this.style.display = "none"; 
+                }
 
+            });
+        });
+    });
 
-
-
-
-
-
-
-
-
-
-    
 }());
